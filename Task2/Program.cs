32829b7b07985b3c884f39f5c28674c2c0118186
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-05-29 16:46:54 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object lockObj = new object();
+
+            Thread thread2 = new Thread(() => ReadAndWrite("File2.txt", "File3.txt", lockObj));
+            Thread thread1 = new Thread(() => ReadAndWrite("File1.txt", "File3.txt", lockObj));
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static void ReadAndWrite(string from, string to, object lockObj)
+        {
+            FileInfo fileInfoFrom = new FileInfo(from);
+            FileInfo fileInfoTo = new FileInfo(to);
+            StreamWriter writer = null;
+            StreamReader reader = null;
+            lock (lockObj)
+            {
+                Thread.Sleep(100);
+                try
+                {
+                    reader = fileInfoFrom.OpenText();
+                    writer = new StreamWriter(fileInfoTo.Open(FileMode.Append, FileAccess.Write));
+                    writer.WriteLine($"{reader.ReadToEnd()} - { DateTime.Now}");
+                    writer.Flush();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    reader?.Close();
+                    writer?.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static int counter = 0;
+
+        static readonly object block = new object();
+
+        private static void Function()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 10; ++i)
+                {
+                    Thread.Sleep(20);
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - {++counter}");
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Thread[] threads = { new Thread(Function), new Thread(Function), new Thread(Function) };
+
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
